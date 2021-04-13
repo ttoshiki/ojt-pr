c055af2ee23d7f979ecd3af29b0f7aab0669d114
t-tamaru <t-tamaru@sakura.ad.jp>
2021-04-13 18:07:10 +0900

    completeページはいらない

--- page-complete.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<div class="complete-wrapper">
-  <h1 class="complete-header">登録完了</h1>
-  <p class="complete-paragraph">事前登録が完了しました。</p>
-  <a href="<?php echo home_url('/seminar/pr設計編/') ?>" class="complete-link">会員限定ページへ</a>
-</div>
-
-<?php get_footer(); ?>
